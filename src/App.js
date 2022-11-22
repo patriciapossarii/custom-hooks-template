957a6e82413c19e24b0d54ebb8636b657653f6dc
patriciapossarii <patricia.mag.carvalho@gmail.com>
2022-11-22 09:36:43 -0300

    template

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { BASE_URL } from "./constants/constants";
+import axios from "axios";
+
+function App() {
+  const [nomeUsuarios, setNomeUsuarios] = useState([]);
+  const [postagens, setPostagens] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}users`)
+      .then((response) => {
+        setNomeUsuarios(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}comments`)
+      .then((response) => {
+        setPostagens(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+
+  return (
+    <>
+      <p>Exercício 1</p>
+      {nomeUsuarios.map((usuario) => {
+        return <p key={usuario.id}>{usuario.name}</p>;
+      })}
+      <hr />
+      <p>Exercício 2</p>
+      {postagens.map((post) => {
+        return <p key={post.id}>{post.body}</p>;
+      })}
+    </>
+  );
+}
+
+export default App;
